421df85707f4c32158f85294d91f96c528de1fc0
vooolkan <69401347+vooolkan@users.noreply.github.com>
2020-08-10 17:17:55 -0500

    first commit

--- src/git_project002/My_repository.java
+++ src/git_project002/My_repository.java
@@ -3,7 +3,8 @@ package git_project002;
 public class My_repository {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		system.out.println(" in the remote repository");
+		
 
 	}
 
