f73b5c764058329baa7a4e2c4b5767db60c54318
vooolkan <voolkanyagci@gmail.com>
2020-08-10 22:59:49 -0500

    before transfer remote repository

--- src/git_project002/My_repository.java
+++ src/git_project002/My_repository.java
@@ -4,6 +4,8 @@ public class My_repository {
 
 	public static void main(String[] args) {
 		System.out.println(" in the remote repository");
+		System.out.println("git push -u origin master denemesi");
+		
 		
 
 	}
