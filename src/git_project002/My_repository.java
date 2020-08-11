e6dbe84a7f13feabda3b6fbee28c905de4b4a849
vooolkan <voolkanyagci@gmail.com>
2020-08-10 23:04:58 -0500

    only git push -u origin master doesnt work

--- src/git_project002/My_repository.java
+++ src/git_project002/My_repository.java
@@ -5,7 +5,7 @@ public class My_repository {
 	public static void main(String[] args) {
 		System.out.println(" in the remote repository");
 		System.out.println("git push -u origin master denemesi");
-		
+		System.out.println("git add . / git commit -m  message proje stage area tasimadan ");
 		
 
 	}
